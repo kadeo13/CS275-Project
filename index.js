b583e0e0538c119a803c9aa0ceded2d4cdb73d08
Brandon Kolle <bk584@drexel.edu>
2019-08-27 12:17:51 -0400

    Add map div

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /*Interactive Map of US States*/
 
-let mapBoxToken = "pk.eyJ1IjoiYmZrb2xsZSIsImEiOiJjanp1MHE1ZDgwYmJxM25wY2pkNGtzenVyIn0.YZP-doaUfSFGlpCkTNdG_A";
-let map = L.map('map').setView([37.8, -96], 4);
+var mapBoxToken = "pk.eyJ1IjoiYmZrb2xsZSIsImEiOiJjanp1MHE1ZDgwYmJxM25wY2pkNGtzenVyIn0.YZP-doaUfSFGlpCkTNdG_A";
+var map = L.map('map').setView([37.8, -96], 4);
