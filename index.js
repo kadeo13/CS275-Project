e928a9d4604393eaf9d1d436ad012fb9ce558ca9
Brandon <bk584@drexel.edu>
2019-08-28 21:06:00 -0400

    Added map power by mapbox

--- index.js
+++
@@ -1,4 +0,0 @@
-/*Interactive Map of US States*/
-
-var mapBoxToken = "pk.eyJ1IjoiYmZrb2xsZSIsImEiOiJjanp1MHE1ZDgwYmJxM25wY2pkNGtzenVyIn0.YZP-doaUfSFGlpCkTNdG_A";
-var map = L.map('map').setView([37.8, -96], 4);
---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+/*Interactive Map of US States*/
+
+var mapBoxToken = "pk.eyJ1IjoiYmZrb2xsZSIsImEiOiJjanp1MHE1ZDgwYmJxM25wY2pkNGtzenVyIn0.YZP-doaUfSFGlpCkTNdG_A";
+var usMap = L.map('map').setView([37.8, -96], 4);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: mapBoxToken
+}).addTo(usMap);
