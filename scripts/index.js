06e846d0ce45dce4454d9643ba1d729a4d25e062
Brandon Kolle <bk584@drexel.edu>
2019-08-29 11:50:37 -0400

    Added map interactivity

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,9 @@
 import statesData from '../data/us-states.js';
 
 var mapBoxToken = "pk.eyJ1IjoiYmZrb2xsZSIsImEiOiJjanp1MHE1ZDgwYmJxM25wY2pkNGtzenVyIn0.YZP-doaUfSFGlpCkTNdG_A";
-var usMap = L.map('map').setView([37.8, -96], 4);
+var usMap = L.map('map').setView([37.8, -96], 4); //Main map object
 
+//Add map tiling service
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
@@ -15,4 +16,42 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 	accessToken: mapBoxToken
 }).addTo(usMap);
 
-L.geoJson(statesData).addTo(usMap);
+var geojson;
+
+//Border highlighting 
+function highlightFeature(e) {
+	var layer = e.target;
+
+	layer.setStyle({
+		weight: 5,
+		color: '#666',
+		dashArray: '',
+		fillOpacity: 0.7
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+		layer.bringToFront();
+	}
+}
+
+//Un-highlight on mouseout
+function resetHighlight(e) {
+	geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+	map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
+		click: zoomToFeature
+	});
+}
+
+
+geojson = L.geoJson(statesData, {
+	onEachFeature: onEachFeature
+}).addTo(usMap);
